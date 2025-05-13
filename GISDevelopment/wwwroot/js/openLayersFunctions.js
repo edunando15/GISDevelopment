1ac5edd21e87c83d1615a77c0934c7cf64632734
Eduardo Tiano <eduardovincenz.tiano@studenti.unicam.it>
2025-05-13 12:48:47 +0200

    [add] JavaScript function to display points. [edit] Web pages (refactoring of function name).

--- GISDevelopment/wwwroot/js/openLayersFunctions.js
+++ GISDevelopment/wwwroot/js/openLayersFunctions.js
@@ -1,4 +1,4 @@
-﻿function initMap(center) {
+﻿function initialiseMap(center) {
     map = new ol.Map({
         target: 'map',
         layers: [
@@ -13,14 +13,15 @@
     });
 }
 
-// Function used to load the polygon of a municipality.
-async function loadPolygon(element, id) {
-    const response = await fetch(element + id);
+// Function used to load a polygon, given the API controller to call and the id of the polygon.
+async function loadPolygon(controller, id) {
+    const response = await fetch(controller + id);
     if(!response.ok) {
         console.error("Failed to load polygon with id " + id + ".");
         return;
     }
     const wkt = await response.text();
+    console.log(wkt);
     var format = new ol.format.WKT();
     var feature = format.readFeature(wkt, {
         dataProjection: 'EPSG:4326',
@@ -41,4 +42,46 @@ async function loadPolygon(element, id) {
         })
     });
     map.addLayer(vectorLayer);
+}
+
+// Function used to load a point, given the API controller to call and the id of the point.
+async function loadPoint(controller, id){
+    response = await fetch(controller + id);
+    if(!response.ok) {
+        console.error("Failed to load point with id " + id + ".");
+        return;
+    }
+    wkt = await response.text();
+    console.log(wkt);
+    var format = new ol.format.WKT();
+    var feature = format.readFeature(wkt, {
+        dataProjection: 'EPSG:3857',
+        featureProjection: 'EPSG:3857'
+    });
+    var vectorSource = new ol.source.Vector({
+        features: [feature]
+    });
+    var vectorLayer = new ol.layer.Vector({
+        source: vectorSource,
+        style: new ol.style.Style({
+            image: new ol.style.Circle({
+                radius: 2,
+                fill: new ol.style.Fill({
+                    color: 'rgba(0, 0, 255, 0.3)'
+                }),
+                fill: new ol.style.Fill({
+                    color: 'blue'
+                }),
+                stroke: new ol.style.Stroke({
+                    color: 'blue',
+                    width: 2
+                })
+            })
+        })
+    });
+    map.addLayer(vectorLayer);
+    // Zoom to the point.
+    const geometry = feature.getGeometry();
+    console.log('Geometry: ', geometry);
+    map.getView().fit(geometry, {padding: [50, 50, 50, 50], maxZoom: 8});
 }
